50d6eb8ed585e5262b5a67b79a19a0b43b913957
Salman_JS <salman4javascript@gmail.com>
2022-12-19 23:16:47 +0530

    brew-date some functions added

--- brewDate/Date.js
+++ brewDate/Date.js
@@ -57,6 +57,142 @@ const formatDate = (props) => {
   return event.toDateString();
 }
 
+// Number of days between two dates!
+const diffBetween = (props) => {
+  const date1 = new Date(props.props1);
+  const date2 = new Date(props.props2);
+  const diffTime = Math.abs(date2 - date1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+  return diffDays;
+}
+
+// Subtract dates by number
+const subDates = (props) => {
+  const date = new Date(props.date);
+  date.setDate(date.getDate() - props.number)
+  return date.toDateString();
+}
+
+// Add dates by numbers 
+const addDates = (props) => {
+  const date = new Date(props.date);
+  date.setDate(date.getDate() + props.number)
+  return date.toDateString();
+}
+
+// Number of hours between current date and the provided date! 
+const ago = (props) => {
+    const date = new Date();
+    const propsDate = new Date(props);
+    var diff = (propsDate.getTime() - date.getTime()) / 1000;
+    diff /= (60 * 60);
+    return Math.abs(Math.round(diff));
+}
+
+// Get day by date 
+const getDay = (props, format) => {
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const date = new Date(props);
+  const day = days[date.getDay()];
+  if(format == "dddd" || format == undefined){
+    return day;
+  } else if(format == "ddd"){
+    return date.toString().split(' ')[0];
+  } else {
+    return "Please check your format!";
+  }
+}
+
+// Number of seconds between two dates!
+const diffSeconds = (props) => {
+  if(props.date2 == undefined){
+    const date2 = Date.now();
+    const date1 = new Date(props.date1).getTime();
+    // 1 second = 1000 ms;
+    const diff = Math.floor((date2 - date1) / 1000);
+    return Math.abs(diff);
+  } else {
+    const date1 = new Date(props.date1).getTime();
+    const date2 = new Date(props.date2).getTime();
+    // 1 second = 1000 ms;
+    const diff = Math.floor((date2 - date1) / 1000);
+    return Math.abs(diff);
+  }
+}
+
+// Number of minutes between two dates!
+const diffMinutes = (props) => {
+  if(props.date2 == undefined){
+    props[props.date2] = new Date();
+    let seconds = diffSeconds(props);
+    // 1 minute = 60 seconds!
+    let minutesDiff = Math.floor(seconds / 60);
+    return Math.abs(minutesDiff);
+  } else {
+    let seconds = diffSeconds(props);
+    // 1 minute = 60 seconds!
+    let minutesDiff = Math.floor( seconds / 60 );
+    return Math.abs(minutesDiff);
+  }
+}
+
+// Number of hours between two dates!
+const diffHours = (props) => {
+  const minutes = diffMinutes(props);
+  // 1 hour = 60 minutes!
+  const diffHours = Math.floor(minutes / 60);
+  return Math.abs(diffHours);
+}
+
+// Number of days between two dates!
+const diffDays = (props) => {
+  const hours = diffHours(props);
+  // 1 day  = 24 hours!
+  const diffDays = Math.floor( hours / 24);
+  return Math.abs(diffDays);
+}
+
+// Number of week between two dates!
+const diffWeeks = (props) => {
+  const weeks = diffDays(props);
+  // 1 week = 7 days!
+  const diffWeeks = Math.floor( weeks / 7);
+  return Math.abs(diffWeeks);
+}
+
+// Number of years between two dates!
+const diffYears = (props) => {
+  if(props.date2 == undefined){
+    const date1 = new Date(props.date1);
+    const date2 = new Date();
+    const yearsDiff = date2.getFullYear() - date1.getFullYear();
+    return Math.abs(yearsDiff);
+  } else {
+    const date1 = new Date(props.date1);
+    const date2 = new Date(props.date2);
+    const yearsDiff = date2.getFullYear() - date1.getFullYear();
+    return Math.abs(yearsDiff);
+  }
+}
+
+// Number of month between two dates!
+const diffMonths = (props) => {
+  const years = diffYears(props);
+  const date1 = new Date(props.date1);
+  if(props.date2 == undefined){
+    const date2 = new Date();
+    const months = (years * 12) + (date2.getMonth() - date1.getMonth());
+    return Math.abs(months); 
+  } else {
+    const date2 = new Date(props.date2)
+    const months = (years * 12) + (date2.getMonth() - date1.getMonth());
+    return Math.abs(months);
+  }
+}
+
+
 module.exports = {
-  getDate, getFullDate, format, formatDate
+  getDate, getFullDate, format, formatDate, diffBetween, 
+  subDates, addDates, ago, getDay, diffSeconds, diffMinutes, 
+  diffHours, diffDays, diffWeeks, diffYears, diffMonths
 }
--- server.js
+++ server.js
@@ -20,7 +20,137 @@ const formatDate = (year, month, date) => {
   return brewDate.formatDate(props);
 };
 
+// Difference between two dates 
+const diffBetween = (date1, date2) => {
+  const props = {
+    props1 : date1,
+    props2 : date2
+  }
+  return brewDate.diffBetween(props) + " days ago";
+};
+
+// Subtract dates by number
+const subDates = (date, number) => {
+  const props = {
+    date : date,
+    number : number
+  }
+  return brewDate.subDates(props);
+}
+
+// Add dates by number 
+const addDates = (date, number) => {
+  const props = {
+    date : date,
+    number : number 
+  }
+  return brewDate.addDates(props);
+}
+
+// Get difference in hours 
+const ago = (props) => {
+  return brewDate.ago(props) + " hours ago";
+}
+
+// Get the day of the provided date! 
+const getDay = (props, format) => {
+  return brewDate.getDay(props, format);
+}
+
+// Number of seconds between two dates!
+const diffSeconds = (date1, date2) => {
+  const props = {
+    date1 : date1,
+    date2 : date2
+  }
+  if(date2 == undefined){
+    return brewDate.diffSeconds(props) + " seconds ago";
+  }
+  return brewDate.diffSeconds(props) + " seconds";
+}
+
+// Number of minutes between two dates!
+const diffMinutes = (date1, date2) => {
+  const props = {
+    date1 : date1,
+    date2 : date2
+  }
+  if(date2 == undefined){
+    return brewDate.diffMinutes(props) + " minutes ago";
+  } else {
+    return brewDate.diffMinutes(props) + " minutes";
+
+  }
+}
+
+// Number of hours between two dates!
+const diffHours = (date1, date2) => {
+  const props = {
+    date1 : date1,
+    date2 : date2
+  }
+  if(date2 == undefined){
+    return brewDate.diffHours(props) + " hours ago";
+  } else {
+    return brewDate.diffHours(props) + " hours";
+  }
+}
+
+// Number of days between two dates!
+const diffDays = (date1, date2) => {
+  const props = {
+    date1 : date1,
+    date2 : date2
+  }
+  if(date2 == undefined){
+    return brewDate.diffDays(props) + " days ago";
+  } else {
+    return brewDate.diffDays(props) + " days";
+  }
+}
+
+// Number of weeks between two dates!
+const diffWeeks = (date1, date2) => {
+  const props = {
+    date1 : date1,
+    date2 : date2
+  }
+  if(date2 == undefined){
+    return brewDate.diffWeeks(props) + " weeks ago";
+  } else {
+    return brewDate.diffWeeks(props) + " weeks";
+  }
+}
+
+// Number of years between two dates!
+const diffYears = (date1, date2) => {
+  const props = {
+    date1 : date1,
+    date2 : date2
+  }
+  if(date2 == undefined){
+    return brewDate.diffYears(props) + " years ago";
+  } else {
+    return brewDate.diffYears(props) + " years";
+  }
+}
+
+// Number of months between two dates!
+const diffMonths = (date1, date2) => {
+    const props = {
+      date1:  date1,
+      date2: date2
+    }
+    if(date2 == undefined){
+      return brewDate.diffMonths(props) + " months ago";
+    } else {
+      return brewDate.diffMonths(props) + " months";
+    }
+}
+
 // Exporting the functions
 module.exports = {
-  getDate, getFullDate, format, formatDate
+  getDate, getFullDate, format, formatDate, diffBetween, 
+  subDates, addDates, ago, getDay, diffSeconds, 
+  diffMinutes, diffHours, diffDays, diffWeeks, diffYears, diffMonths
 }
